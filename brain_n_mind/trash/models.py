e6e30fada69fe6d77a2f3eb4a35c00706d874298
AryanKhurana <64011374+AryanKhurana@users.noreply.github.com>
2021-05-22 10:37:31 +0530

    Have added all models with required fields and attributes.

--- brain_n_mind/trash/models.py
+++ brain_n_mind/trash/models.py
@@ -1,3 +1,68 @@
+from django.contrib.auth.models import User
+from django.conf import settings
 from django.db import models
+from django.shortcuts import reverse
+LEVEL_CHOICES=((1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5),
+(6,6),
+(7,7),
+(8,8),
+(9,9))
+ITEM_CHOICES1=((0,0),
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5),
+(6,6),
+(7,7),
+(8,8),
+(9,9))
+ITEM_CHOICES2=((0,0),
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5),
+(6,6),
+(7,7),
+(8,8),
+(9,9))
+ITEM_CHOICES=3((0,0),
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5),
+(6,6),
+(7,7),
+(8,8),
+(9,9))
+class Profile(models.Model):
+    first=models.CharField(max_length=100,blank=False,null=False)
+    last=models.CharField(max_length=100,blank=False,null=False)
+    img=models.ImageField(default="enviro.png",upload_to="/profilepics")
+    email=models.EmailField(max_length=200,blank=True)
+    bio=models.TextField(default="Save Earth!",,blank=True)
+    user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    country=models.CharField(max_length=200,blank=True)
+class Level(models.Model):
+    user=models.OneToOneField(Profile)
+    number=models.IntegerField(default=1,choices=LEVEL_CHOICES)
 
-# Create your models here.
+
+class Reduce(models.Model):
+    user_reduce=models.OneToOneField(Profile)
+    filled_reduce=models.IntegerField(default=0,choices=ITEM_CHOICES1)
+    text_reduce=models.ArrayField(models.TextField(max_length=400))
+class Reuse(models.Model):
+    user_reuse=models.OneToOneField(Profile)
+    filled_reuse=models.IntegerField(default=0,choices=ITEM_CHOICES2)
+    text_reuse=models.ArrayField(models.TextField(max_length=400))
+class Recycle(models.Model):
+    user_recycle=models.OneToOneField(Profile)
+    filled_recycle=models.IntegerField(default=0,choices=ITEM_CHOICES3)
+    text_recycle=models.ArrayField(models.TextField(max_length=400))
