70687bb8c9c4484769967f5f86405364316e27ee
Anupamladdha <56990426+Anupamladdha@users.noreply.github.com>
2021-05-22 14:31:51 +0530

    Fixed issues temporarily in models.py also added allauth files and some styling
    Changed models.py so that there is no error but check models.py.
    Added allauth files and the styling which i used for our wt project

--- brain_n_mind/brain_n_mind/urls.py
+++ brain_n_mind/brain_n_mind/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
+    url('accounts/', include('allauth.urls')),
     path('app/', include('trash.urls'))
 ]
